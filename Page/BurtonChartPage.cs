708e711b0b766e4977e719e16bc9e6bc5b175abe
VitalijaIJ <93917255+VitalijaIJ@users.noreply.github.com>
2021-11-24 08:56:29 +0100

    Update BurtonChartPage.cs

--- Page/BurtonChartPage.cs
+++ Page/BurtonChartPage.cs
@@ -1,4 +1,4 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using System;
@@ -26,7 +26,8 @@ namespace AutomatinisTestavimas.Page
         private IWebElement ItemTotal => Driver.FindElement(By.CssSelector(".item-total"));
         private IWebElement DropDownClick => Driver.FindElement(By.Id("dwfrm_cart_shipments_i0_items_i0_quantity"));
         private SelectElement ChangeQnt => new SelectElement(Driver.FindElement(By.Id("dwfrm_cart_shipments_i0_items_i0_quantity")));
-        private IWebElement Result => Driver.FindElement(By.CssSelector("#dwfrm_cart > div > div > div.item-qty-summary > div.item-summary > p > span"));
+        private IWebElement Result => Driver.FindElement(By.CssSelector(".order-total > div"));
+      
         public BurtonChartPage ItemPrice()
         {
             Assert.IsTrue(ItemTotal.Text.Equals("$39.95"));
@@ -37,11 +38,19 @@ namespace AutomatinisTestavimas.Page
             DropDownClick.Click();           
             return this;        
         }
-       
         public BurtonChartPage ChangeQntTo1oClick(string text)
         {
-            ChangeQnt.SelectByValue("10");
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => DropDownClick.Displayed);
+            ChangeQnt.SelectByValue(text);
             return this;
-        }       
+        }
+          public BurtonChartPage CheckItemTotalAmount(string total) //$399.50
+          {
+              WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+              wait.Until(d => Result.Displayed);
+              Assert.IsTrue(Result.Text.Contains(total), "Amount is not correct");
+              return this;
+          }  
     }
 }
